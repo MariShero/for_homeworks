e1a4e282c4e7b4591aec6bfdb1abd2f34f8c1734
MariShero <127594624+MariShero@users.noreply.github.com>
2023-05-04 21:54:31 +0400

    added new file

---
+++ შუალედური_2(შესწორებული).py
@@ -0,0 +1,48 @@
+#1
+import sqlite3
+conn = sqlite3.connect("survey.sqlite")
+cursor = conn.cursor()
+
+result = cursor.execute("SELECT count(id) FROM students where SelfStudyHour <2")
+for row in result:
+    list(row)
+    print(row[0])
+
+def amount(device, age):
+    result = cursor.execute("SELECT count(id) FROM students WHERE Device = ? AND Age = ?",(device, age))
+    for row in result:
+        list(row)
+        print(row[0])
+
+x = amount("Smartphone", 20)
+
+cursor.execute("""INSERT INTO students(Age, OnlineClassTime, Device, SelfStudyHour, FitnessTime, Sleep, SocialMedia, SocialMediaPlatform) VALUES(15, 2, "Laptop", 3, 1, 9, 3, "youtube");""")
+conn.commit()
+
+result = cursor.execute("""UPDATE students SET SocialMediaPlatform = "MARIAM-gram" WHERE Age <> 21""")
+conn.commit()
+
+conn.close()
+
+#2
+import json
+
+file = open("sample.json", "r+")
+data = json.load(file)
+
+print(data["person"]["address"])
+
+x = data["person"]["friends"]
+z = []
+for i in range(len(x)):
+    z.append(data["person"]["friends"][i]["name"])
+print(z)
+
+a = data["person"]
+try:
+   key = input("შეიყვანეთ ველის სახელი: ")
+   print(a[key])
+except KeyError:
+   print("თქვენს მიერ შეყვანილი ველი არ არსებობს")
+
+file.close()
